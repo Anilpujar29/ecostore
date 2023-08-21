c3cb65e046a9479d1f11cae93c11dd83ed8c7181
pujara <anilkumar.pujar@netcom-kassel.de>
2023-08-21 14:26:33 +0200

    Changes in YML file

--- JavaClient/src/main/java/com/cgi/ecostore/HttpClient.java
+++ JavaClient/src/main/java/com/cgi/ecostore/HttpClient.java
@@ -20,6 +20,9 @@ public class HttpClient implements CommandLineRunner {
 
     @Autowired
     private EnvironmentProperties environment;
+    private Boolean alarm;
+    private int occupancy;
+    private int occupancy1;
 
     private final Logger logger = LoggerFactory.getLogger("HttpClient");
 
@@ -35,6 +38,11 @@ public class HttpClient implements CommandLineRunner {
     private void run() throws InterruptedException {
         var devices = environment.getDevices();
         var url = environment.getUrl();
+
+        this.alarm = true;
+        this.occupancy = 8;
+        this.occupancy1 = 6;
+
 
         while (true) {
             devices.forEach(
@@ -79,19 +87,66 @@ public class HttpClient implements CommandLineRunner {
         String[] fields = telemetry.split(",");
 
         for (String field : fields) {
-            String trimmedField = field.trim();
-            payloadBuilder.append("\"").append(trimmedField).append("\":\"").append(getValue(device.getRange()))
+
+            String trimmedField = field.trim(); {
+
+            if(this.alarm && field.equals("temperature") ){
+                payloadBuilder.append("\"").append(trimmedField).append("\":\"").append("81")
+                        .append("\",");
+                this.alarm = false;
+                System.out.println("Alarm data sent.....");}
+
+
+            payloadBuilder.append("\"").append(trimmedField).append("\":\"").append(getValue(device.getRange(), device))
                     .append("\",");
         }
 
-        payloadBuilder.deleteCharAt(payloadBuilder.length() - 1); // Remove the trailing comma
+
+    }
+
+        payloadBuilder.deleteCharAt(payloadBuilder.length()-1); // Remove the trailing comma
         payloadBuilder.append("}");
 
         return HttpRequest.BodyPublishers.ofString(payloadBuilder.toString());
     }
 
-    private String getValue(EnvironmentProperties.Range range) {
-        int r = (int) ((Math.random() * (range.getMax()) - range.getMin()) + range.getMin());
+    private String getValue(EnvironmentProperties.Range range, EnvironmentProperties.Device device) {
+        if(device.getName().equals("Room1 occupancy") ) {
+            double prob = (Math.random());
+            int delta;
+            if(prob > .6) {
+                delta = 1;
+            }
+            else if (prob < .4) {
+                delta= -1;
+            }
+            else delta = 0;
+            occupancy += delta;
+            if (occupancy<0) {
+                occupancy=0;
+            }
+            return String.valueOf(occupancy);
+
+        }
+        if(device.getName().equals("Room2 occupancy") ) {
+            double prob = (Math.random());
+            int delta;
+            if(prob > .6) {
+                delta = 1;
+            }
+            else if (prob < .4) {
+                delta= -1;
+            }
+            else delta = 0;
+            occupancy1 += delta;
+            if (occupancy1<0) {
+                occupancy1=0;
+            }
+            return String.valueOf(occupancy1);
+
+        }
+        int r = (int) ((Math.random() * (range.getMax() - range.getMin())) + range.getMin());
+        if(device.getName().equals("Energy Meter Alpha Storage")) System.out.println(device.getRange().getMax() + " " +device.getRange().getMin()+ " " + r);
         return String.valueOf(r);
     }
 
